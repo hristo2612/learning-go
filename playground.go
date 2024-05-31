523beb7fb04da0f60e50fc25a6ad30e57e446ce9
hristo2612 <hristoapps@gmail.com>
2024-05-31 13:49:56 +0300

    Tutorial 5 to 9. Module 1 complete

--- playground.go
+++ playground.go
@@ -15,6 +15,14 @@ func main() {
 	tutorial4()
 	fmt.Println()
 	tutorial5()
+	fmt.Println()
+	tutorial6()
+	fmt.Println()
+	tutorial7()
+	fmt.Println()
+	tutorial8()
+	fmt.Println()
+	tutorial9()
 }
 
 func tutorial1() {
@@ -27,8 +35,8 @@ func add(x int, y int) int {
 }
 
 func tutorial2() {
-	fmt.Println("2. Functions can take zero or more arguments:")
-	fmt.Println(`func add(x int, y int) int { 
+	fmt.Println(`2. Functions can take zero or more arguments:
+func add(x int, y int) int { 
  return x + y
 }
 add(5, 10)`)
@@ -40,8 +48,8 @@ func add2(x int, y int) int {
 }
 
 func tutorial3() {
-	fmt.Println("3. When two or more consecutive arguments share the same type we can add the type only to the last argument like so:")
-	fmt.Println(`func add(x, y int) int { 
+	fmt.Println(`3. When two or more consecutive arguments share the same type we can add the type only to the last argument like so:
+func add(x, y int) int { 
  return x + y
 }
 add(5, 10)`)
@@ -53,8 +61,8 @@ func swap(x, y string) (string, string) {
 }
 
 func tutorial4() {
-	fmt.Println("4. A function can return any number of results:")
-	fmt.Println(`func swap(x, y string) (string, string) {
+	fmt.Println(`4. A function can return any number of results:
+func swap(x, y string) (string, string) {
  return y, x
 }
 a, b := swap("X", "Y")`)
@@ -64,5 +72,38 @@ a, b := swap("X", "Y")`)
 }
 
 func tutorial5() {
+	var python, java, csharp bool
+	fmt.Println(`5. The 'var' statement declares a list of variables. The type is last as in function arguments:
+var python, java, csharp bool
+fmt.Println(python, java, csharp)
+Result:`, python, java, csharp)
+}
 
+func tutorial6() {
+	var python, java, csharp = true, 4, "csharp"
+	fmt.Println(`6. The 'var' declaration can also include initialisers like so. The type is inferred from the value:
+var python, java, csharp = true, 4, "csharp"
+fmt.Println(python, java, csharp)
+Result:`, python, java, csharp)
+}
+
+func tutorial7() {
+	fmt.Println(`7. Inside a function instead of declaring vars with the 'var' keyword we can use := directly like so:
+var i = 3
+j := 3
+Those are the same thing.`)
+}
+
+func tutorial8() {
+	fmt.Println(`8. The expression T(v) converts value 'v' to type 'T':
+someIntNumber := 5
+someFloatNumber := float64(someIntNumber)`)
+	someIntNumber := 5
+	someFloatNumber := float64(someIntNumber)
+	fmt.Println(someFloatNumber)
+}
+
+func tutorial9() {
+	fmt.Println(`9. Constant variables are defined with 'const' keyword:
+const penguin = "penguin"`)
 }
